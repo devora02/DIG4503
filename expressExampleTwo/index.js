386e828b89b856ee6b68954c48a2ddeea53232dd
devora02 <devoraamiel@yahoo.com>
2020-01-30 19:30:51 -0500

    LAB 3 DONE

--- expressExampleTwo/index.js
+++ expressExampleTwo/index.js
@@ -2,7 +2,7 @@ const Express = require("express");
 const App = Express();
 const port = 80;
 
-App.use("/client", Express.static("client/build"));
+App.use("/static", Express.static("static"));
 
 App.get("/api", function(req, res) {
     res.send("Something else, I guess?")
